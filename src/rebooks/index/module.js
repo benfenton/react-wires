d51e6fccc845c49774dcc022dae2029c00c02b9e
benfenton <benfenton@gmail.com>
2014-12-29 03:29:58 -0500

    correct module loading

---
+++ src/rebooks/index/module.js
@@ -0,0 +1,10 @@
+var React = require('react');
+
+
+mymodule = React.createClass({
+	render: function(foo) {
+		var blackColor = {color: "red"};
+		return <li style={blackColor}> {this.props} </li>;
+	}
+});
+module.exports = mymodule
